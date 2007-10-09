1d70aec534e3b0ef7eb300fa3b218084fdea7f3e
cmsbuild <>
2007-10-09 19:14:09 +0000

    This commit was manufactured by cvs2git to create tag 'DataFormats- Provenance-CMSSW_1_6_12'.
    Sprout from master 2007-10-09 19:14:03 UTC William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch> 'Revert to 1_6_X'
    Cherrypick from master 2007-03-15 21:45:37 UTC William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch> 'Support backward compatibility':
        interface/BranchType.h
        interface/ProcessConfiguration.h
        src/classes.h
    Delete:
        interface/FileID.h
        interface/Selections.h
        src/FileID.cc

--- interface/ProcessConfiguration.h
+++ interface/ProcessConfiguration.h
@@ -1,9 +1,8 @@
 #ifndef DataFormats_Provenance_ProcessConfiguration_h
 #define DataFormats_Provenance_ProcessConfiguration_h
 
-#include <iosfwd>
 #include <string>
-#include <vector>
+#include <iosfwd>
 
 #include "DataFormats/Provenance/interface/ParameterSetID.h"
 #include "DataFormats/Provenance/interface/PassID.h"
@@ -26,16 +25,13 @@ namespace edm {
     ParameterSetID const& parameterSetID() const {return parameterSetID_;}
     ReleaseVersion const& releaseVersion() const {return releaseVersion_;}
     PassID const& passID() const {return passID_;}
-    ProcessConfigurationID id() const;
 
     std::string processName_;
     ParameterSetID parameterSetID_;
     ReleaseVersion releaseVersion_; 
     PassID passID_;
 
-    typedef std::vector<std::string> ModulesOnPath_t;
-    std::map<std::string, ModulesOnPath_t> module_names_by_path_;
-    //std::map<std::string, ?> 
+    ProcessConfigurationID id() const;
   };
 
   inline
